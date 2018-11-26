21056f06b7490a1f371c601dd7c1aea4fb1867cf
duxing <515235416@qq.com>
2018-11-26 15:34:12 +0800

    mpv小程序初始化

---
+++ src/pages/index/index.vue
@@ -0,0 +1,54 @@
+<template>
+<div >
+    <van-button type="primary">主要按钮</van-button>
+    <van-cell-group>
+        <van-cell title="单元格" value="内容" />
+        <van-cell title="单元格" is-link />
+    </van-cell-group>
+    <van-cell-group>
+        <van-field
+            v-model="username"
+            clearable
+            label="用户名"
+            placeholder="请输入用户名"
+        />
+    <van-field
+            v-model="username"
+            clearable
+            label="用户名"
+            icon="arrow"
+            placeholder="请输入用户名"
+        /> 
+            </van-cell-group>
+    
+
+  
+</div>
+</template>
+<script>
+    import Toast from '@/../static/vant/toast/toast'
+    export default {
+        data() {
+            return {
+               
+            }
+        },
+        components: {
+
+        },
+        methods: {
+            close_e() {
+                Toast.fail('失败文案');
+
+            }
+        },
+        mounted() {
+
+        },
+    }
+
+</script>
+<style scoped>
+
+
+</style>
